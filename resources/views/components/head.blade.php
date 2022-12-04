7a43f9728049baef6662ea78ef2fbc8c4d6f0ea9
repto-ia <iasheyam@gmail.com>
2022-12-04 06:35:24 -0700

    deploy

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -2,7 +2,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>LetsGeo</title>
+    <link rel="icon" type="image/x-icon" href="{{asset('/images/logo.png')}}">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -12,6 +13,6 @@
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     <!-- Styles -->
-    <link rel="stylesheet" type="text/css" href="app.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('app.css')}}">
 
 </head>
