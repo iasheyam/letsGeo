2745d327134be73d938a1c9498eacfb16436312b
repto-ia <iasheyam@gmail.com>
2022-12-04 20:44:26 -0700

    favicon update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-{{--        <meta name="csrf-token" content="{{ csrf_token() }}">--}}
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-{{--        <title>{{ config('app.name', 'Laravel') }}</title>--}}
+        <title>LetsGeo</title>
+        <link rel="icon" type="image/x-icon" href="{{asset('/images/logo.png')}}">
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
